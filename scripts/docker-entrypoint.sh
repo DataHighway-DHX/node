da7897ec647656738b61e48bb1abc871f10b5d0e
Christian Gröschel <christian.groeschel@gmail.com>
2020-03-06 11:37:56 +0000

    exposing websocket and rpc ports

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -7,4 +7,6 @@
   --node-key 88dc3417d5058ec4b4503e0c12ea1a0a89be200fe98922423d4334014fa6b0ee \
   --alice \
   --port 30333 \
+  --ws-port 9944 \
+  --rpc-port 9933 \
   --telemetry-url ws://telemetry.polkadot.io:1024
