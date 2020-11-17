1ae50ab2649f5e16fef53eb87a86f31cdaf32b15
Christian Groeschel <christian@mxc.org>
2020-11-17 15:50:37 +0100

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,10 +4,9 @@ set -e
 
 echo "*** Initializing WASM build environment"
 
-if [ -z $CI_PROJECT_NAME ] ; then
-  rustup update stable
-  rustup toolchain install nightly-2020-10-06
-  rustup default nightly-2020-10-06
-fi
-
+rustup update stable
+rustup update nightly
+rustup toolchain install nightly-2020-10-06
 rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
+rustup default nightly-2020-10-06
+rustup override set nightly-2020-10-06
