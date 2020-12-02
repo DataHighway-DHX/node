ad49d6c56f5d850ab465dc7c1ae8ab19c08ed686
Christian Gröschel <christian.groeschel@gmail.com>
2020-12-02 10:00:40 +0100

    update docker entrypoint scripts

--- scripts/docker-entrypoint-node-2.sh
+++ scripts/docker-entrypoint-node-2.sh
@@ -5,7 +5,7 @@
   --unsafe-rpc-external \
   --rpc-cors=all \
   --base-path /tmp/polkadot-chains/node-2 \
-  --bootnodes /dns4/${ENDPOINT_TESTNET}/tcp/30333/p2p/${BOOTNODE_ID_NODE_TESTNET_1} \
+  --bootnodes /dns4/${ENDPOINT_TESTNET}/tcp/30333/p2p/${BOOTNODE_ID_TESTNET} \
   --keystore-path "/tmp/polkadot-chains/node-2/keys" \
   --chain ../node/src/chain-definition-custom/chain_def_${CHAIN_VERSION}.json \
   --name "${NODE_ENV} Validator Node 2" \
@@ -14,5 +14,5 @@
   --rpc-port 9933 \
   --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
   --execution=native \
-  -lruntime=debug
+  -lruntime=debug \
   --rpc-methods=Unsafe
