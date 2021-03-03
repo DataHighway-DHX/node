0795dbd68ea866d04fd41abae4b0d4721a521826
Ilya Beregovskiy <festeloqq@gmail.com>
2021-03-03 10:24:20 +0200

    exchange pallet - set config tests

--- pallets/exchange-rate/src/tests.rs
+++ pallets/exchange-rate/src/tests.rs
@@ -78,3 +78,67 @@ fn transfer_handles_basic_errors() {
         assert_eq!(ExchangeRateTestModule::exchange_rate_owner(0), Some(1));
     });
 }
+
+#[test]
+fn set_config_defaults_works() {
+    new_test_ext().execute_with(|| {
+        // Setup
+        assert_ok!(ExchangeRateTestModule::create(Origin::signed(1)));
+        // Call Functions
+        assert_ok!(ExchangeRateTestModule::set_config(Origin::signed(1), 0, None, None, None, None, None));
+        // Verify Storage
+        assert_eq!(ExchangeRateTestModule::exchange_rate_count(), 1);
+        assert!(ExchangeRateTestModule::exchange_rates(0).is_some());
+        assert_eq!(ExchangeRateTestModule::exchange_rate_owner(0), Some(1));
+
+        assert!(ExchangeRateTestModule::exchange_rate_configs(0).is_some());
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().hbtc, 200000);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().dot, 100);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().iota, 5);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().fil, 200);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().decimals_after_point, 2);
+    });
+}
+
+#[test]
+fn set_config_works() {
+    new_test_ext().execute_with(|| {
+        // Setup
+        assert_ok!(ExchangeRateTestModule::create(Origin::signed(1)));
+        // Call Functions
+        assert_ok!(ExchangeRateTestModule::set_config(Origin::signed(1), 0, Some(777), Some(778), None, Some(779), Some(3)));
+        // Verify Storage
+        assert_eq!(ExchangeRateTestModule::exchange_rate_count(), 1);
+        assert!(ExchangeRateTestModule::exchange_rates(0).is_some());
+        assert_eq!(ExchangeRateTestModule::exchange_rate_owner(0), Some(1));
+
+        assert!(ExchangeRateTestModule::exchange_rate_configs(0).is_some());
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().hbtc, 777);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().dot, 778);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().iota, 5);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().fil, 779);
+        assert_eq!(ExchangeRateTestModule::exchange_rate_configs(0).unwrap().decimals_after_point, 3);
+    });
+}
+
+#[test]
+fn et_config_basic_errors() {
+    new_test_ext().execute_with(|| {
+        // Setup
+        assert_ok!(ExchangeRateTestModule::create(Origin::signed(1)));
+        // Call Functions
+        assert_noop!(
+            ExchangeRateTestModule::set_config(Origin::signed(1), 1, None, None, None, None, None),
+            "ExchangeRates does not exist"
+        );
+        assert_noop!(
+            ExchangeRateTestModule::set_config(Origin::signed(2), 0, None, None, None, None, None),
+            "Only owner can set exchange_rate_config"
+        );
+        // Verify Storage
+        assert_eq!(ExchangeRateTestModule::exchange_rate_count(), 1);
+        assert!(ExchangeRateTestModule::exchange_rates(0).is_some());
+        assert_eq!(ExchangeRateTestModule::exchange_rate_owner(0), Some(1));
+    });
+}
+
