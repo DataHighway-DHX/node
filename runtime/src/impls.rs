d0e9b6ef17ef293a2d20a258241360414542a6a2
Luke Schoen <ltfschoen@gmail.com>
2021-03-30 13:59:42 +0200

    fix indentation

--- runtime/src/impls.rs
+++ runtime/src/impls.rs
@@ -22,7 +22,7 @@ use crate::{Balances, Authorship, NegativeImbalance};
 
 pub struct Author;
 impl OnUnbalanced<NegativeImbalance> for Author {
-	fn on_nonzero_unbalanced(amount: NegativeImbalance) {
-		Balances::resolve_creating(&Authorship::author(), amount);
-	}
+    fn on_nonzero_unbalanced(amount: NegativeImbalance) {
+        Balances::resolve_creating(&Authorship::author(), amount);
+    }
 }
