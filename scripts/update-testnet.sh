a4d99daa79b2962be4dc819cee8677f2c7906baa
Luke Schoen <ltfschoen@users.noreply.github.com>
2021-04-20 22:00:23 +1000

    update docs and fix directory (#176)

--- scripts/update-testnet.sh
+++ scripts/update-testnet.sh
@@ -4,5 +4,5 @@ set -e
 
 cargo clean
 
-WASM_BUILD_TYPE=release cargo run -- build-spec --chain local > ./node/src/chain-spec-templates/chain_spec_local.json
-WASM_BUILD_TYPE=release cargo run -- build-spec --chain ./node/src/chain-spec-templates/chain_spec_local.json --raw > ./node/src/chain-built/chain_def_local.json
+WASM_BUILD_TYPE=release cargo run -- build-spec --chain local > ./node/src/chain-built/chain_spec_local.json
+WASM_BUILD_TYPE=release cargo run -- build-spec --chain ./node/src/chain-built/chain_spec_local.json --raw > ./node/src/chain-built/chain_def_local.json
