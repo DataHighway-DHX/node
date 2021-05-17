92455a3bb3287775d554416928071563c133660e
Christian Gröschel <christian.groeschel@gmail.com>
2021-05-17 14:44:15 +0200

    update init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ echo "*** Initializing WASM build environment"
 
 rustup update stable
 rustup update nightly
-rustup toolchain install nightly-2020-12-12
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-12-12
-rustup default nightly-2020-12-12
-rustup override set nightly-2020-12-12
+rustup toolchain install nightly-2021-03-10
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-10
+rustup default nightly-2021-03-10
+rustup override set nightly-2021-03-10
