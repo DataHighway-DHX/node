95ebaa6554fc6ab73b4c3e1d5761820dd70f3f46
Luke Schoen <ltfschoen@gmail.com>
2021-05-28 15:59:22 +0200

    fix missing harbour testnet keys

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -501,11 +501,11 @@ pub fn datahighway_testnet_harbour_config() -> Result<ChainSpec, String> {
                     // babe
                     hex!["f2bf53bfe43164d88fcb2e83891137e7cf597857810a870b4c24fb481291b43a"]
                         .unchecked_into(),
-                    // im_online - FIXME: missing from lastpass
-                    hex!["504cf9eb92f9e992a15370d2536063df83b69744ad4502e05dc91e3dae3b2649"]
+                    // im_online
+                    hex!["ee834a837b99eb433e48bd64e5604e3a5a7f782e9ca11311832cd8b89a2dd010"]
                         .unchecked_into(),
-                    // authority_discovery - FIXME: missing from lastpass
-                    hex!["7941e1c7ff93c9541a779b40780715fe6211407c970906f3b554b294a6ba7ec7"]
+                    // authority_discovery
+                    hex!["ae84ff3133f827bd8e5185f34aae89658f86f1ae13e401de2fa3703e7a6a3c6d"]
                         .unchecked_into(),
                 ),
                 // authority #2
@@ -520,11 +520,11 @@ pub fn datahighway_testnet_harbour_config() -> Result<ChainSpec, String> {
                     // babe
                     hex!["1e91a7902c89289f97756c4e20c0e9536f34de61c7c21af7773d670b0e644030"]
                         .unchecked_into(),
-                    // im_online - FIXME: missing from lastpass
-                    hex!["65be4fe3c1efba2b10f05b50bdb25cce394ed9c30ab5ebf2d604c0628d8500e7"]
+                    // im_online
+                    hex!["267cff7dc3de158a584e2c20647006b1e753e1df13b65d4e725c8e23575ff74c"]
                         .unchecked_into(),
-                    // authority_discovery - FIXME: missing from lastpass
-                    hex!["48a3726bef363c990585cb550da2b1d5e309ae6a0ea1d4d06bf389bce303dae8"]
+                    // authority_discovery
+                    hex!["8243b8b785a10949839e1958c5d77f9d954912f734cbfb3e0048810e8e0eb20b"]
                         .unchecked_into(),
                 ),
                 // authority #3
@@ -539,11 +539,11 @@ pub fn datahighway_testnet_harbour_config() -> Result<ChainSpec, String> {
                     // babe
                     hex!["aaabcb653ce5dfd63035430dba10ce9aed5d064883b9e2b19ec5d9b26a457f57"]
                         .unchecked_into(),
-                    // im_online - FIXME: missing from lastpass
-                    hex!["76a21eefe9824ae269d4fc6609e310f323053acd2ef47fe36e6d0be75a5cd75b"]
+                    // im_online
+                    hex!["2003bccdbc7d1016ee647856b6cf178fbb9a0f36d841dc9483a39f63cd912e0e"]
                         .unchecked_into(),
-                    // authority_discovery - FIXME: missing from lastpass
-                    hex!["138b33e1bd40d51e95455c19a9558ba4b5db4969943183592b10227537366a8e"]
+                    // authority_discovery
+                    hex!["bc2e58bf62bc0f49a6a324cd93920f01619a8917d3d9c960f4201cb6cf11721b"]
                         .unchecked_into(),
                 ),
                 // authority #4
@@ -558,30 +558,30 @@ pub fn datahighway_testnet_harbour_config() -> Result<ChainSpec, String> {
                     // babe
                     hex!["a49ac1053a40a2c7c33ffa41cb285cef7c3bc9db7e03a16d174cc8b5b5ac0247"]
                         .unchecked_into(),
-                    // im_online - FIXME: missing from lastpass
-                    hex!["2bb62b29a6c80c32a9284026044483caf85b699744bc155e8c1a0c5819578f4b"]
+                    // im_online
+                    hex!["a6e8669d0916754c654f1adb57d7b28a8f1a7a58794f9bdd4334135458cbab5c"]
                         .unchecked_into(),
-                    // authority_discovery - FIXME: missing from lastpass
-                    hex!["a3ca5592ca7de669469a94c7ff6f64d8e2fcc5ab226833420748d25fab56bbe0"]
+                    // authority_discovery
+                    hex!["06140939946d86bd67d4fc3ab8eee4a9bddd669cd464ace4f5256aa4d9469e19"]
                         .unchecked_into(),
                 ),
                 // authority #5
                 (
                     // stash
-                    hex!["ca181fc1f02a0aa144885d3b6f95d333a3a84ecc448b4d9f3541b26d21729168"].into(),
+                    hex!["181319cac9b915a33586196cb1ed64b7f37f9d50ae9f5c04b0d92ecdb342cd0a"].into(),
                     // cont
-                    hex!["f406b4141e7cab5b09e670c617ab65e911da684e4deb76d0d29e94f77a535b39"].into(),
-                    // FIXME: missing from lastpass
-                    hex!["9edf290adfc576f4de8b90a09b3b378263f34748f201a1966153f26a879e5a39"]
+                    hex!["80c17c61a1f2f6252b23677510cbd0c3f3ad1ad8694dba9170e98ab430100a7f"].into(),
+                    // gran
+                    hex!["a240fbd4575d03b4c62e2d2d546327e393db5fc508bc92203fba354a3232f006"]
                         .unchecked_into(),
-                    // FIXME: missing from lastpass
-                    hex!["03ead710287b634d6cdf2db7be3815a48a612fd2bec3e812c6cbe3721d01e756"]
+                    // babe
+                    hex!["2ed0676546bc839b77af6e2b084b549403081b1840d4d777a8623f2c44bd0d3e"]
                         .unchecked_into(),
-                    // im_online - FIXME: missing from lastpass
-                    hex!["d47678b3604e1d61301790e505606ba05d601180b541d0495adac79ff43a5e41"]
+                    // im_online
+                    hex!["5209213d7d09853295062d63199cb272888a678a4a2bc52aefd62248b6ff9c02"]
                         .unchecked_into(),
-                    // authority_discovery - FIXME: missing from lastpass
-                    hex!["f96cb52e84e128ba23a7627b154f015c15f54acd31881522e505e28076b8dc66"]
+                    // authority_discovery
+                    hex!["ae641bd93c58d5235054b3a498794b1d18213b9818b7c12eaec596dcb283235f"]
                         .unchecked_into(),
                 ),
             ],
@@ -604,40 +604,40 @@ pub fn datahighway_testnet_harbour_config() -> Result<ChainSpec, String> {
                 hex!["ca907b74f921b74638eb40c289e9bf1142b0afcdb25e1a50383ab8f9d515da0d"].into(),
                 hex!["6a9da05f3e07d68bc29fb6cf9377a1537d59f082f49cb27a47881aef9fbaeaee"].into(),
                 hex!["f2bf53bfe43164d88fcb2e83891137e7cf597857810a870b4c24fb481291b43a"].into(),
-                hex!["504cf9eb92f9e992a15370d2536063df83b69744ad4502e05dc91e3dae3b2649"].into(),
-                hex!["7941e1c7ff93c9541a779b40780715fe6211407c970906f3b554b294a6ba7ec7"].into(),
+                hex!["ee834a837b99eb433e48bd64e5604e3a5a7f782e9ca11311832cd8b89a2dd010"].into(),
+                hex!["ae84ff3133f827bd8e5185f34aae89658f86f1ae13e401de2fa3703e7a6a3c6d"].into(),
 
                 // authority #2
                 hex!["420a7b4a8c9f2388eded13c17841d2a0e08ea7c87eda84310da54f3ccecd3931"].into(),
                 hex!["ae69db7838fb139cbf4f93bf877faf5bbef242f3f5aac6eb4f111398e9385e7d"].into(),
                 hex!["9af1908ac74b042f4be713e10dcf6a2def3770cfce58951c839768e7d6bbcd8e"].into(),
                 hex!["1e91a7902c89289f97756c4e20c0e9536f34de61c7c21af7773d670b0e644030"].into(),
-                hex!["65be4fe3c1efba2b10f05b50bdb25cce394ed9c30ab5ebf2d604c0628d8500e7"].into(),
-                hex!["48a3726bef363c990585cb550da2b1d5e309ae6a0ea1d4d06bf389bce303dae8"].into(),
+                hex!["267cff7dc3de158a584e2c20647006b1e753e1df13b65d4e725c8e23575ff74c"].into(),
+                hex!["8243b8b785a10949839e1958c5d77f9d954912f734cbfb3e0048810e8e0eb20b"].into(),
 
                 // authority #3
                 hex!["ceecb6cc08c20ff44052ff19952a810d08363aa26ea4fb0a64a62a4630d37f28"].into(),
                 hex!["7652b25328d78d264aef01184202c9771b55f5b391359309a2559ef77fbbb33d"].into(),
                 hex!["b8902681768fbda7a29666e1de8a18f5be3c778d92cf29139959a86e6bff13e7"].into(),
                 hex!["aaabcb653ce5dfd63035430dba10ce9aed5d064883b9e2b19ec5d9b26a457f57"].into(),
-                hex!["76a21eefe9824ae269d4fc6609e310f323053acd2ef47fe36e6d0be75a5cd75b"].into(),
-                hex!["138b33e1bd40d51e95455c19a9558ba4b5db4969943183592b10227537366a8e"].into(),
+                hex!["2003bccdbc7d1016ee647856b6cf178fbb9a0f36d841dc9483a39f63cd912e0e"].into(),
+                hex!["bc2e58bf62bc0f49a6a324cd93920f01619a8917d3d9c960f4201cb6cf11721b"].into(),
 
                 // authority #4
                 hex!["68bac5586028dd40db59a7becec349b42cd4229f9d3c31875c3eb7a57241cd42"].into(),
                 hex!["eec96d02877a45fa524fcee1c6b7c849cbdc8cee01a95f5db168c427ae766849"].into(),
                 hex!["f4807d86cca169a81d42fcf9c7abddeff107b0a73e9e7a809257ac7e4a164741"].into(),
                 hex!["a49ac1053a40a2c7c33ffa41cb285cef7c3bc9db7e03a16d174cc8b5b5ac0247"].into(),
-                hex!["2bb62b29a6c80c32a9284026044483caf85b699744bc155e8c1a0c5819578f4b"].into(),
-                hex!["a3ca5592ca7de669469a94c7ff6f64d8e2fcc5ab226833420748d25fab56bbe0"].into(),
+                hex!["a6e8669d0916754c654f1adb57d7b28a8f1a7a58794f9bdd4334135458cbab5c"].into(),
+                hex!["06140939946d86bd67d4fc3ab8eee4a9bddd669cd464ace4f5256aa4d9469e19"].into(),
 
                 // authority #5
-                hex!["ca181fc1f02a0aa144885d3b6f95d333a3a84ecc448b4d9f3541b26d21729168"].into(),
-                hex!["f406b4141e7cab5b09e670c617ab65e911da684e4deb76d0d29e94f77a535b39"].into(),
-                hex!["9edf290adfc576f4de8b90a09b3b378263f34748f201a1966153f26a879e5a39"].into(),
-                hex!["03ead710287b634d6cdf2db7be3815a48a612fd2bec3e812c6cbe3721d01e756"].into(),
-                hex!["d47678b3604e1d61301790e505606ba05d601180b541d0495adac79ff43a5e41"].into(),
-                hex!["f96cb52e84e128ba23a7627b154f015c15f54acd31881522e505e28076b8dc66"].into(),
+                hex!["181319cac9b915a33586196cb1ed64b7f37f9d50ae9f5c04b0d92ecdb342cd0a"].into(),
+                hex!["80c17c61a1f2f6252b23677510cbd0c3f3ad1ad8694dba9170e98ab430100a7f"].into(),
+                hex!["a240fbd4575d03b4c62e2d2d546327e393db5fc508bc92203fba354a3232f006"].into(),
+                hex!["2ed0676546bc839b77af6e2b084b549403081b1840d4d777a8623f2c44bd0d3e"].into(),
+                hex!["5209213d7d09853295062d63199cb272888a678a4a2bc52aefd62248b6ff9c02"].into(),
+                hex!["ae641bd93c58d5235054b3a498794b1d18213b9818b7c12eaec596dcb283235f"].into(),
             ],
 			true,
 		),
