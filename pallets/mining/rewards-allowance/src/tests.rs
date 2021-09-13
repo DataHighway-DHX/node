d3ab8b65ee2ee65885664ef9f378d58b49e2e48b
Luke Schoen <ltfschoen@gmail.com>
2021-09-13 18:57:15 +0200

    tests: try to emit event with pre-image hash but no System events

--- pallets/mining/rewards-allowance/src/tests.rs
+++ pallets/mining/rewards-allowance/src/tests.rs
@@ -1,7 +1,10 @@
 use crate::{mock::*, Error};
 use crate::{BondedDHXForAccountData};
 use codec::Encode;
-use frame_support::{assert_noop, assert_ok};
+use frame_support::{assert_noop, assert_ok,
+    weights::{DispatchClass, DispatchInfo, GetDispatchInfo},
+};
+use frame_system::{self, AccountInfo, EventRecord, Phase};
 use sp_core::{
     H256,
     Hasher, // so we may use BlakeTwo256::hash
@@ -89,20 +92,62 @@ fn setup_preimage() {
         match Democracy::note_preimage(Origin::signed(1), encoded_proposal_preimage.clone()) {
             Ok(_) => (),
             Err(x) if x == Error::<Test>::DuplicatePreimage.into() => (),
-            Err(x) => panic!("note_preimage error {:?}", x),
+            Err(x) => panic!("Democracy::note_preimage error {:?}", x),
         }
+        assert_eq!(System::events(),
+            vec![
+                EventRecord {
+                    phase: Phase::ApplyExtrinsic(0),
+                    event: Default::default(),
+                    topics: vec![],
+                }
+            ]
+        );
         System::set_block_number(1);
         // add a public proposal with a pre-image hash (with a required deposit)
         // to the proposal queue prior to it becoming a referendum
-        let p1 = Democracy::propose(Origin::signed(1), BlakeTwo256::hash(b"test"), 1);
-        // add a single proposal with a pre-image hash (no deposit required)
-        // that originates from an external origin (i.e. collective group)
-        // to the external queue prior to it becoming a referendum
-        let p2 = Democracy::external_propose(Origin::signed(1), BlakeTwo256::hash(b"test"));
+        //
+        // since we've configured MinimumDeposit to be 100 * DOLLARS, which is 100_000_000_000_000,
+        // we need to deposit at least that much otherwise we get error `ValueLow`
+        match Democracy::propose(Origin::signed(1), BlakeTwo256::hash(b"test"), 100_000_000_000_000) {
+            Ok(_) => (),
+            Err(x) => panic!(" Democracy::propose error {:?}", x),
+        }
+
+        // // add a single proposal with a pre-image hash (no deposit required)
+        // // that originates from an external origin (i.e. collective group)
+        // // to the external queue prior to it becoming a referendum
+        // match Democracy::external_propose(Origin::signed(3), BlakeTwo256::hash(b"test")) {
+        //     Ok(_) => (),
+        //     Err(x) => panic!(" Democracy::external_propose error {:?}", x),
+        // }
+
+        // the above returns `BadOrigin`
+
         System::set_block_number(2);
         Democracy::note_imminent_preimage(Origin::signed(1), encoded_proposal_preimage.clone());
-        // second the proposals
+        let public_prop_count = Democracy::public_prop_count();
+        assert_eq!(public_prop_count, 1);
+        // check if a lock exists on an account until a block in the future.
+        // there shouldn't be any yet
+        let locks_until_block_for_account = Democracy::locks(1);
+        assert_eq!(locks_until_block_for_account, None);
         assert_ok!(Democracy::second(Origin::signed(1), 0, u32::MAX));
+        // check the deposits made on a proposal index
+		let deposits = Democracy::deposit_of(0).ok_or("Proposal not created").unwrap();
+		assert_eq!(deposits.0.len(), 2 as usize, "Seconds not recorded");
+
+        // let preimage = <Preimages<T>>::take(&proposal_hash);
+
+        // // second the proposals
+        // assert_ok!(Democracy::second(Origin::signed(1), 0, u32::MAX));
+
+        // let referendum_count = Democracy::referendum_count();
+        // assert_eq!(referendum_count, Some(0));
+
+        // // info about a referendum index
+        // let referendum_info_1 = Democracy::referendum_info(0);
+        // assert_eq!(referendum_info_1, None);
 
         // thread 'tests::setup_preimage' panicked at 'Expected Ok(_). Got Err(
         //     Module {
@@ -114,42 +159,42 @@ fn setup_preimage() {
         //     },
         // )', pallets/mining/rewards-allowance/src/tests.rs:105:9
 
-        assert_ok!(Democracy::second(Origin::signed(1), 1, u32::MAX));
-        // we have 4.32 seconds per block, with a launch period of 672 hours,
-        // so there are 10450944 blocks in the launch period before the the
-        // public and external proposals take turns becoming a referendum
-        System::set_block_number(11_000_000);
-        // wait for referendums to be launched from the proposals after the launch period
-		// external proposal becomes referendum first
-		assert_eq!(
-			Democracy::referendum_status(0),
-			Ok(ReferendumStatus {
-				end: 11_000_020, // block when voting on referendum ends
-				proposal_hash: BlakeTwo256::hash(b"test"),
-				threshold: VoteThreshold::SuperMajorityApprove,
-				delay: 2,
-				tally: Tally { ayes: 0, nays: 0, turnout: 0 },
-			})
-		);
-		// public proposal's turn to become a referendum
-		assert_eq!(
-			Democracy::referendum_status(1),
-			Ok(ReferendumStatus {
-				end: 11_000_020,
-				proposal_hash: BlakeTwo256::hash(b"test"),
-				threshold: VoteThreshold::SuperMajorityApprove,
-				delay: 2,
-				tally: Tally { ayes: 0, nays: 0, turnout: 0 },
-			})
-		);
-        System::set_block_number(11_000_001);
-        // end of voting on referendum
-        System::set_block_number(11_000_050);
-        // vote on referenda using time-lock voting with a conviction to scale the vote power
-        // note: second parameter is the referendum index
-		assert_ok!(Democracy::vote(Origin::signed(1), 0, aye(1)));
-        assert_eq!(tally(0), Tally { ayes: 1, nays: 0, turnout: 10 });
-		assert_ok!(Democracy::vote(Origin::signed(1), 1, aye(1)));
-        assert_eq!(tally(1), Tally { ayes: 1, nays: 0, turnout: 10 });
+
+        // // we have 4.32 seconds per block, with a launch period of 672 hours,
+        // // so there are 10450944 blocks in the launch period before the the
+        // // public and external proposals take turns becoming a referendum
+        // System::set_block_number(11_000_000);
+        // // wait for referendums to be launched from the proposals after the launch period
+		// // external proposal becomes referendum first
+		// assert_eq!(
+		// 	Democracy::referendum_status(0),
+		// 	Ok(ReferendumStatus {
+		// 		end: 11_000_020, // block when voting on referendum ends
+		// 		proposal_hash: BlakeTwo256::hash(b"test"),
+		// 		threshold: VoteThreshold::SuperMajorityApprove,
+		// 		delay: 2,
+		// 		tally: Tally { ayes: 0, nays: 0, turnout: 0 },
+		// 	})
+		// );
+		// // public proposal's turn to become a referendum
+		// assert_eq!(
+		// 	Democracy::referendum_status(1),
+		// 	Ok(ReferendumStatus {
+		// 		end: 11_000_020,
+		// 		proposal_hash: BlakeTwo256::hash(b"test"),
+		// 		threshold: VoteThreshold::SuperMajorityApprove,
+		// 		delay: 2,
+		// 		tally: Tally { ayes: 0, nays: 0, turnout: 0 },
+		// 	})
+		// );
+        // System::set_block_number(11_000_001);
+        // // end of voting on referendum
+        // System::set_block_number(11_000_050);
+        // // vote on referenda using time-lock voting with a conviction to scale the vote power
+        // // note: second parameter is the referendum index
+		// assert_ok!(Democracy::vote(Origin::signed(1), 0, aye(1)));
+        // assert_eq!(tally(0), Tally { ayes: 1, nays: 0, turnout: 10 });
+		// assert_ok!(Democracy::vote(Origin::signed(1), 1, aye(1)));
+        // assert_eq!(tally(1), Tally { ayes: 1, nays: 0, turnout: 10 });
 	});
 }
