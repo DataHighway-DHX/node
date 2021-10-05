52db6c30bf00c8ac6666f1a8fc5285ae43002658
Ayush <>
2021-10-05 16:58:02 +0530

    Addressed PR comment

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -112,7 +112,7 @@ pub fn development_config(id: ParaId) -> ChainSpec {
 
 pub fn local_testnet_config(id: ParaId) -> ChainSpec {
     let mut properties = sc_chain_spec::Properties::new();
-    properties.insert("tokenSymbol".into(), "DHX".into());
+    properties.insert("tokenSymbol".into(), "UNIT".into());
     properties.insert("tokenDecimals".into(), 18.into());
     ChainSpec::from_genesis(
         // Name
@@ -144,57 +144,6 @@ pub fn local_testnet_config(id: ParaId) -> ChainSpec {
         Vec::new(),
         None,
         None,
-        Some(properties),
-        Extensions {
-            relay_chain: "rococo-local".into(),
-            para_id: id.into(),
-        },
-    )
-}
-
-pub fn harbor_testnet_config(id: ParaId) -> ChainSpec {
-    let mut properties = sc_chain_spec::Properties::new();
-    properties.insert("tokenSymbol".into(), "DHX".into());
-    properties.insert("tokenDecimals".into(), 18.into());
-    ChainSpec::from_genesis(
-        // Name
-        "Local Testnet",
-        // ID
-        "local_testnet",
-        ChainType::Local,
-        move || {
-            testnet_genesis(
-                hex!["3c917f65753cd375582a6d7a1612c8f01df8805f5c8940a66e9bda3040f88f5d"].into(),
-                vec![
-                        hex!["f64bae0f8fbe2eb59ff1c0ff760a085f55d69af5909aed280ebda09dc364d443"].unchecked_into(),
-                        hex!["ca907b74f921b74638eb40c289e9bf1142b0afcdb25e1a50383ab8f9d515da0d"].unchecked_into(),
-                        hex!["6a9da05f3e07d68bc29fb6cf9377a1537d59f082f49cb27a47881aef9fbaeaee"].unchecked_into(),
-                        hex!["f2bf53bfe43164d88fcb2e83891137e7cf597857810a870b4c24fb481291b43a"].unchecked_into(),
-                        hex!["420a7b4a8c9f2388eded13c17841d2a0e08ea7c87eda84310da54f3ccecd3931"].unchecked_into(),
-                        hex!["ae69db7838fb139cbf4f93bf877faf5bbef242f3f5aac6eb4f111398e9385e7d"].unchecked_into(),
-                        hex!["9af1908ac74b042f4be713e10dcf6a2def3770cfce58951c839768e7d6bbcd8e"].unchecked_into(),
-                        hex!["1e91a7902c89289f97756c4e20c0e9536f34de61c7c21af7773d670b0e644030"].unchecked_into(),
-                ],
-                vec![
-                    // Endow this account with the DHX DAO Unlocked Reserves Balance
-                    // 5EWKojw2i3uoqfWx1dEgVjBsvK5xuTr5G3NjXYh47H6ycBWr
-                    hex!["6c029e6fc41ec44d420030071f04995bac19e59a0f0a1a610f9f0f6d689e2262"].into(),
-                    // Endow these accounts with a balance so they may bond as authorities
-                    hex!["ca907b74f921b74638eb40c289e9bf1142b0afcdb25e1a50383ab8f9d515da0d"].into(),
-                    hex!["ae69db7838fb139cbf4f93bf877faf5bbef242f3f5aac6eb4f111398e9385e7d"].into(),
-                    hex!["7652b25328d78d264aef01184202c9771b55f5b391359309a2559ef77fbbb33d"].into(),
-                    hex!["eec96d02877a45fa524fcee1c6b7c849cbdc8cee01a95f5db168c427ae766849"].into(),
-                    hex!["f64bae0f8fbe2eb59ff1c0ff760a085f55d69af5909aed280ebda09dc364d443"].into(),
-                    hex!["420a7b4a8c9f2388eded13c17841d2a0e08ea7c87eda84310da54f3ccecd3931"].into(),
-                    hex!["ceecb6cc08c20ff44052ff19952a810d08363aa26ea4fb0a64a62a4630d37f28"].into(),
-                    hex!["68bac5586028dd40db59a7becec349b42cd4229f9d3c31875c3eb7a57241cd42"].into(),
-                ],
-                id,
-            )
-        },
-        vec![],
-        None,
-        Some("dhx-test"),
         Some(properties),
         Extensions {
             relay_chain: "rococo-local".into(),
--- node/src/command.rs
+++ node/src/command.rs
@@ -42,7 +42,6 @@ fn load_spec(id: &str, para_id: ParaId) -> std::result::Result<Box<dyn sc_servic
     Ok(match id {
         "dev" => Box::new(chain_spec::development_config(para_id)),
         "" | "local" => Box::new(chain_spec::local_testnet_config(para_id)),
-        "harbour" => Box::new(chain_spec::harbor_testnet_config(para_id)),
         "rococo" => Box::new(chain_spec::rococo_parachain_config(para_id)),
         path => Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
     })
