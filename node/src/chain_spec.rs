586f23d2ad0c474b0b05ad3b2fc5090f20c88878
Ayush <>
2021-10-07 16:19:56 +0530

    Addressed PR comments

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -158,8 +158,8 @@ pub fn rococo_parachain_config(id: ParaId) -> ChainSpec {
     properties.insert("tokenDecimals".into(), 18.into());
     let boot_nodes = vec![];
     ChainSpec::from_genesis(
-        "DataHighway",
-        "datahighway",
+        "DataHighway Spreehafen Parachain Testnet",
+        "datahighway_spreehafen",
         ChainType::Live,
         move || {
             spreehafen_testnet_genesis(
