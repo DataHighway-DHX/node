cf202d292021e9882b45c733400356060cf5f162
Luke Schoen <ltfschoen@gmail.com>
2022-03-04 11:39:50 +0100

    update rust version necessary

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ echo "*** Initializing WASM build environment"
 
 rustup update stable
 rustup update nightly
-rustup toolchain install nightly-2021-08-31
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-08-31
-rustup default nightly-2021-08-31
-rustup override set nightly-2021-08-31
+rustup toolchain install nightly-2021-12-01
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-12-01
+rustup default nightly-2021-12-01
+rustup override set nightly-2021-12-01
