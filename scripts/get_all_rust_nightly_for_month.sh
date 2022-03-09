a67c7943000bd8f557c491d7c8ab65da88b9866e
Luke Schoen <ltfschoen@gmail.com>
2022-03-09 08:28:49 +0100

    update to substrate commit 50156013c9316cd8849cb3db34670d046b8d66ea

--- scripts/get_all_rust_nightly_for_month.sh
+++ scripts/get_all_rust_nightly_for_month.sh
@@ -2,7 +2,7 @@
 
 # install all available nightly versions for a given month
 month=12
-for ((day=15; day<=31; day++))
+for ((day=22; day<=31; day++))
 do
   rustup toolchain install nightly-2021-$month-$day
 done
