598f948b9e5a7ee66bcc19b876061a504258ab54
Luke Schoen <ltfschoen@gmail.com>
2022-03-09 16:50:02 +0100

    update to substrate commit 7e643ceabf11858cd3c059579ab45794793f2a78

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1014,6 +1014,83 @@ impl pallet_bags_list::Config for Runtime {
 	type BagThresholds = BagThresholds;
 }
 
+parameter_types! {
+    pub const VoteLockingPeriod: BlockNumber = 30 * DAYS;
+}
+
+impl pallet_conviction_voting::Config for Runtime {
+    type WeightInfo = pallet_conviction_voting::weights::SubstrateWeight<Self>;
+    type Event = Event;
+    type Currency = Balances;
+    type VoteLockingPeriod = VoteLockingPeriod;
+    type MaxVotes = ConstU32<512>;
+    type MaxTurnout = frame_support::traits::TotalIssuanceOf<Balances, Self::AccountId>;
+    type Polls = Referenda;
+}
+
+parameter_types! {
+    pub const AlarmInterval: BlockNumber = 1;
+    pub const SubmissionDeposit: Balance = 100 * DOLLARS;
+    pub const UndecidingTimeout: BlockNumber = 28 * DAYS;
+}
+
+pub struct TracksInfo;
+impl pallet_referenda::TracksInfo<Balance, BlockNumber> for TracksInfo {
+    type Id = u8;
+    type Origin = <Origin as frame_support::traits::OriginTrait>::PalletsOrigin;
+    fn tracks() -> &'static [(Self::Id, pallet_referenda::TrackInfo<Balance, BlockNumber>)] {
+        static DATA: [(u8, pallet_referenda::TrackInfo<Balance, BlockNumber>); 1] = [(
+            0u8,
+            pallet_referenda::TrackInfo {
+                name: "root",
+                max_deciding: 1,
+                decision_deposit: 10,
+                prepare_period: 4,
+                decision_period: 4,
+                confirm_period: 2,
+                min_enactment_period: 4,
+                min_approval: pallet_referenda::Curve::LinearDecreasing {
+                    begin: Perbill::from_percent(100),
+                    delta: Perbill::from_percent(50),
+                },
+                min_turnout: pallet_referenda::Curve::LinearDecreasing {
+                    begin: Perbill::from_percent(100),
+                    delta: Perbill::from_percent(100),
+                },
+            },
+        )];
+        &DATA[..]
+    }
+    fn track_for(id: &Self::Origin) -> Result<Self::Id, ()> {
+        if let Ok(system_origin) = frame_system::RawOrigin::try_from(id.clone()) {
+            match system_origin {
+                frame_system::RawOrigin::Root => Ok(0),
+                _ => Err(()),
+            }
+        } else {
+            Err(())
+        }
+	}
+}
+
+impl pallet_referenda::Config for Runtime {
+    type WeightInfo = pallet_referenda::weights::SubstrateWeight<Self>;
+    type Call = Call;
+    type Event = Event;
+    type Scheduler = Scheduler;
+    type Currency = pallet_balances::Pallet<Self>;
+    type CancelOrigin = EnsureRoot<AccountId>;
+    type KillOrigin = EnsureRoot<AccountId>;
+    type Slash = ();
+    type Votes = pallet_conviction_voting::VotesOf<Runtime>;
+    type Tally = pallet_conviction_voting::TallyOf<Runtime>;
+    type SubmissionDeposit = SubmissionDeposit;
+    type MaxQueued = ConstU32<100>;
+    type UndecidingTimeout = UndecidingTimeout;
+    type AlarmInterval = AlarmInterval;
+    type Tracks = TracksInfo;
+}
+
 pub const MaxVotesAsConst: u32 = 100;
 
 parameter_types! {
@@ -1329,6 +1406,8 @@ construct_runtime!(
         Tips: pallet_tips,
         BagsList: pallet_bags_list,
         ChildBounties: pallet_child_bounties,
+        Referenda: pallet_referenda,
+        ConvictionVoting: pallet_conviction_voting,
         // TODO - why can't i remove `::{Pallet, Call, Storage, Event<T>}` from the following
         // like in this commit https://github.com/paritytech/substrate/commit/6af19fdc47f16bee2901908e34b70d6d7ba80c59
         MembershipSupernodes: membership_supernodes::{Pallet, Call, Storage, Event<T>},
